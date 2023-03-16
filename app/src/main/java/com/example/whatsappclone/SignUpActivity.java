3d8c27a76714897aeaa0d184a6262f9d7aa43a52
Aditya Shinde <adityashinde4580@gmail.com>
2023-03-16 12:49:28 +0530

    Update SignUpActivity.java
    FireBase Database
    Create Process Dialog
    Create a Button click Event for the Sign Up

--- app/src/main/java/com/example/whatsappclone/SignUpActivity.java
+++ app/src/main/java/com/example/whatsappclone/SignUpActivity.java
@@ -2,15 +2,51 @@ package com.example.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.whatsappclone.databinding.ActivitySignUpBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
+
+    private FirebaseAuth mAuth;
+
+    FirebaseDatabase database;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        database = FirebaseDatabase.getInstance();
 
         getSupportActionBar().hide();
+
+        progressDialog = new ProgressDialog(SignUpActivity.this);
+        progressDialog.setTitle("Creating Account");
+        progressDialog.setMessage("We are creating your account");
+
+        binding.btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!binding.txtUserName.getText().toString().isEmpty() && !binding.txtEmail.getText().toString().isEmpty() && !binding.txtPassword.getText().toString().isEmpty())
+                {
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this, "Enter Credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }

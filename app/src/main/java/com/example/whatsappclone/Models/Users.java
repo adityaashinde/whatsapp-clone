128b2da20d620add2461efb694d26168d0becaae
Aditya Shinde <adityashinde4580@gmail.com>
2023-03-18 03:24:58 +0530

    Update Users.java
    added Getter and Setter

--- app/src/main/java/com/example/whatsappclone/Models/Users.java
+++ app/src/main/java/com/example/whatsappclone/Models/Users.java
@@ -24,5 +24,60 @@ public class Users {
         this.mail = mail;
         this.password = password;
     }
-    
+
+    public String getProfilePic() {
+        return profilePic;
+    }
+
+    public void setProfilePic(String profilePic) {
+        this.profilePic = profilePic;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public void setLastMessage(String lastMessage) {
+        this.lastMessage = lastMessage;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
